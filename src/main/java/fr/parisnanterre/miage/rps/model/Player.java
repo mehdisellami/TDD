55a411e5436671e78de1e7bd84e57a613368b9e8
mehdisellami <mehdisellami9@gmail.com>
2020-09-21 23:19:13 +0200

    Suite

--- src/main/java/fr/parisnanterre/miage/rps/model/Player.java
+++ src/main/java/fr/parisnanterre/miage/rps/model/Player.java
@@ -1,5 +1,6 @@
 package fr.parisnanterre.miage.rps.model;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -8,6 +9,9 @@ public class Player {
     int score,x;
     List<RPSEnum> collection;
     Random rn = new Random();
+
+
+    int nbrmouvement;
 
 
 
@@ -20,10 +24,7 @@ public class Player {
     public Player(String nom) {
         this.nom = nom;
 
-
-
-
-        for ( int nbrmouvement =0 ; nbrmouvement<=10 ; nbrmouvement++){
+        for ( nbrmouvement =0 ; nbrmouvement<=10 ; nbrmouvement++){
             int nbralea = rn.nextInt(3) + 1;
 
             if (nbralea==0 && collection.contains(RPSEnum.ROCK)){
@@ -32,30 +33,38 @@ public class Player {
             else if (nbralea==1 && collection.contains(RPSEnum.PAPER)){
                 x=1;
             }
-            else if (nbralea==1 && collection.contains(RPSEnum.PAPER)){
+            else if (nbralea==1 && collection.contains(RPSEnum.SCISSORS)){
                 x=2;
             }
 
-
-
         }
-
-
-
-
-
-
-
 
     }
 
 
+    public String getNom() {
+        return nom;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getNbrmouvement() {
+        return nbrmouvement;
+    }
 
 
+    public RPSEnum getNextMove(){
+        int idx = collection.indexOf(RPSEnum.ROCK);
+        if (idx < 0 || idx+1 == collection.size()) {
+            return null;
+        }
 
+        return collection.get(idx + 1);
+    }
 
-
-
-
-
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
